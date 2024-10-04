97642dd7f25bc655eb5c81ad352ada4d70e299b6
Balajee1626 <158752752+Balajee1626@users.noreply.github.com>
2024-10-04 19:56:45 +0530

    Add files via upload

---
+++ myharish/myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(response):
+    return HttpResponse("<h1><b> This is the home page </b></h1> <br> <p><center>Web Application Development using Django  Framework</center></p>")
+ 
+def about(response):
+    return HttpResponse("<h1><b> This is the about Page </b></h1> <br> <center><p>I am balajee, UG Student in the Deartment of Information Technology of Kamaraj College of Engineering and Technology.</p> <p>I am passionate about coding. My areas of interest are Machine Learning, Deep Learning, IoT, Security and Networks</p></center>")
+def index(request):
+    return render(request, 'index.html')
